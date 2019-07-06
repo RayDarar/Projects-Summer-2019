4c5d3b151f5b1d60f06d5b3cf942ab70eee252f0
Anakharsis9 <180107200@stu.sdu.edu.kz>
2019-07-06 12:11:30 +0600

    Adding user interface to calculator

--- src/Easy/Calculator/Calculator.java
+++ src/Easy/Calculator/Calculator.java
@@ -16,8 +16,9 @@ public class Calculator extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         stage.setTitle("Calculator");
+	stage.setScene(new Scene(root, 240, 305));
         stage.show();
     }
 }
