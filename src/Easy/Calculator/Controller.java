7d7c5bbe6632805de093a907dd49889426fe3293
Anakharsis9 <180107200@stu.sdu.edu.kz>
2019-07-08 23:26:45 +0600

    interface update

---
+++ src/Easy/Calculator/Controller.java
@@ -0,0 +1,42 @@
+package Calculator;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Controller {
+    @FXML
+    private Button historyBut;
+
+
+
+    @FXML
+    void toHistory(){
+        historyBut.setOnAction(event -> {
+            historyBut.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/Calculator/history.fxml"));
+
+            try{
+                loader.load();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        });
+    }
+
+
+}
---
+++ src/Easy/Calculator/Controller2.java
@@ -0,0 +1,37 @@
+package Calculator;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller2 {
+    @FXML
+    private Button backToCalcBut;
+
+    @FXML
+    void toCalc(){
+        backToCalcBut.setOnAction(event -> {
+            backToCalcBut.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/Calculator/calculator.fxml"));
+
+            try{
+                loader.load();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        });
+    }
+}
