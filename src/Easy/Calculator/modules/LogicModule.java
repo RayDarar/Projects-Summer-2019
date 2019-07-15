4774bd9f3feaeb42350296ccb5a2563d61ed0057
Ansar <dfqgth400@gmail.com>
2019-07-15 16:28:44 +0600

    Adding history

--- src/Easy/Calculator/modules/LogicModule.java
+++ src/Easy/Calculator/modules/LogicModule.java
@@ -1,14 +1,18 @@
 package Easy.Calculator.modules;
 
+import java.util.LinkedList;
+
 /**
  * LogicModule
  * 
  */
 
 public class LogicModule {
+    // Singleton items
     private static LogicModule logic;
 
     private LogicModule() {
+        history = new LinkedList<>();
     }
 
     public static LogicModule getInstance() {
@@ -18,8 +22,25 @@ public class LogicModule {
         return logic;
     }
 
+    // History items
+    private class HistoryItem {
+        String expression;
+        double result;
+
+        HistoryItem(String e, double r) {
+            expression = e;
+            result = r;
+        }
+    }
+
+    private LinkedList<HistoryItem> history;
+
+    // Other logic
     public double calculate(String expression) {
-        
         return 0;
+    }
+
+    private void addToHistory(String expression, double result) {
+        history.add(new HistoryItem(expression, result));
     }
 }
