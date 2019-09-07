764580c0775e6e647e01773e7ae9e61e394b084e
Ansar <dfqgth400@gmail.com>
2019-09-07 17:15:35 +0600

    Calculator update

--- src/Easy/Calculator/modules/LogicModule.java
+++ src/Easy/Calculator/modules/LogicModule.java
@@ -12,7 +12,6 @@ public class LogicModule {
     private static LogicModule logic;
 
     private LogicModule() {
-        history = new LinkedList<>();
     }
 
     public static LogicModule getInstance() {
@@ -21,21 +20,6 @@ public class LogicModule {
 
         return logic;
     }
-
-    // History items
-    private class HistoryItem {
-        String expression;
-        double result;
-
-        HistoryItem(String e, double r) {
-            expression = e;
-            result = r;
-        }
-    }
-
-    private LinkedList<HistoryItem> history;
-
-    // Other logic
 
     /**
      * After calculating the expression, we should add it to history
@@ -115,11 +99,6 @@ public class LogicModule {
 
         double result = numStack.pop();
 
-        addToHistory(expression, result);
         return result;
-    }
-
-    private void addToHistory(String expression, double result) {
-        history.add(new HistoryItem(expression, result));
     }
 }
