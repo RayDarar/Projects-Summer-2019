59e8f3e2f935ca45e8965d0699aaf163be2f26a5
RayDarar <dfqgth400@gmail.com>
2019-11-07 13:01:16 +0600

    front-end finished for CSV2JSON

--- src/Easy/CSV2JSON/controller.js
+++ src/Easy/CSV2JSON/controller.js
@@ -1,2 +1,10 @@
 import logic from "./logic.js";
 
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+const convert_btn = document.querySelector(".convert-btn");
+
+convert_btn.onclick = () => {
+    output.value = logic.convert(input.value);
+    
+}
