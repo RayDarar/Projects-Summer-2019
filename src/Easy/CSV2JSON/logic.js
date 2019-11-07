6d84f329e1d8e75185f7975d8e128eb2e3bd8cb0
Dayana <dayana530807@gmail.com>
2019-11-07 13:22:12 +0600

    logic

--- src/Easy/CSV2JSON/logic.js
+++ src/Easy/CSV2JSON/logic.js
@@ -1,3 +1,17 @@
 export default {
-    
-}
+    convert: (csv) => {
+        const array = csv.split("\n");
+        const properties = array[0].split(",");
+        const jsons = [];
+        for (let i = 1; i < array.length; i++) {
+           const item = {};
+           const value = array[i].split(",");
+           for (let j = 0; j < properties.length; j++) {
+               item[properties[j]] = value[j];
+           }
+           jsons.push(item);
+        }
+        return jsons;
+
+    },
+};
