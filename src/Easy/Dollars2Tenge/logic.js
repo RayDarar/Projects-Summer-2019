009b10d99b0946946eb5949f716df8b40e5bf001
RayDarar <dfqgth400@gmail.com>
2019-11-12 13:03:43 +0600

    logic.js writed for dollars-to-tenge

--- src/Easy/Dollars2Tenge/logic.js
+++ src/Easy/Dollars2Tenge/logic.js
@@ -1,3 +1,18 @@
-export default{
-    
+/**
+ * @author RayDarar
+ * 
+ * @description proportion used: 1 dollar = 388.77 tenge
+ * 
+ */
+
+const proportion = 388.77;
+const logic = {
+    convert2Dollars: (tenge) => {
+        return tenge < 1 ? 0 : tenge / proportion;
+    },
+    convert2Tenge: (dollars) => {
+        return dollars < 1 ? 0 : dollars * proportion;
+    }
 }
+
+export default logic;
