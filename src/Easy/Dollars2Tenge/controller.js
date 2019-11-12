28deee38195ddd5b01fd1afaab2b21f61e98dbe2
Dayana <dayana530807@gmail.com>
2019-11-12 13:45:34 +0600

    Dollars2Tenge finished

--- src/Easy/Dollars2Tenge/controller.js
+++ src/Easy/Dollars2Tenge/controller.js
@@ -1,1 +1,11 @@
 import logic from "./logic.js";
+
+const kz = document.querySelector(".kz");
+const usd = document.querySelector(".usd");
+
+kz.onkeyup = (event) => {
+    usd.value = logic.convert2Dollars(kz.value);
+}
+usd.onkeyup = (event) => {
+    kz.value = logic.convert2Tenge(usd.value);
+}
