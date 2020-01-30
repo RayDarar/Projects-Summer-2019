fadfafa8dca53c43b77c971a3dee8f8738823afd
RayDarar <dfqgth400@gmail.com>
2020-01-30 13:52:30 +0600

    message-list: tests written & logger middleware added

--- src/Intermediate/MessageList/src/app.js
+++ src/Intermediate/MessageList/src/app.js
@@ -2,7 +2,21 @@ import { config } from "dotenv";
 config();
 
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(process.env.PWD, "public")));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request on ${req.url} at ${new Date().toTimeString()}`);
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running!");
--- src/Intermediate/MessageList/test/dotenv.js
+++ src/Intermediate/MessageList/test/dotenv.js
@@ -6,4 +6,7 @@ describe("environment", () => {
   it("must have PORT variable defined", () => {
     expect(process.env.PORT).not.to.be.equal(undefined);
   });
+  it("must have secret key defined", () => {
+    expect(process.env.SECRET_KEY).not.to.be.equal(undefined);
+  });
 });
