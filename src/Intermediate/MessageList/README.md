45fcb87ca285c80b2fe22c8f962c81e2d03a6d16
RayDarar <dfqgth400@gmail.com>
2020-01-30 11:44:13 +0600

    readme update & message-list project setup finished

---
+++ src/Intermediate/MessageList/README.md
@@ -0,0 +1,5 @@
+# Message List
+
+The main idea is to practice Vue.js + Node.js on this project. This is the single message list for authenticated users.
+
+## REST Api
---
+++ src/Intermediate/MessageList/src/app.js
@@ -0,0 +1,9 @@
+import { config } from "dotenv";
+config();
+
+import express from "express";
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running!");
+});
